54187e47c2b38ba3a6caa8fd43d960047bd9b0e3
khageshsingh7819-dot <khageshsingh7819@gmail.com>
2026-09-06 00:07:04 +0530

    Add files via upload

---
+++ Q19.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+	int main() {
+	int a,b,c;
+
+	printf("enter three sides:");
+	scanf("%d %d %d", &a,&b,&c);
+
+	if (a==b && b==c)
+	printf("equilateral triangle:");
+	else if (a==b||b==c||a==c)
+	printf("isosceles triangle:");
+	else
+	printf("scalene triangle");
+
+	return 0;
+}
---
+++ Q22.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    float cp, sp, profit, loss;
+
+    printf("Enter Cost Price: ");
+    scanf("%f", &cp);
+
+    printf("Enter Selling Price: ");
+    scanf("%f", &sp);
+
+    if (sp > cp) {
+        profit = ((sp - cp) / cp) * 100;
+        printf("Profit = %.2f%%", profit);
+    }
+    else if (cp > sp) {
+        loss = ((cp - sp) / cp) * 100;
+        printf("Loss = %.2f%%", loss);
+    }
+    else {
+        printf("No Profit, No Loss");
+    }
+
+    return 0;
+}
