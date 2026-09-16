6ab0148b85e5d699c28b84e62cf6074d9ab8e413
khageshsingh7819-dot <khageshsingh7819@gmail.com>
2026-09-17 00:04:28 +0530

    Add files via upload

---
+++ Q59.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, even = 0, odd = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enter array elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        if (a[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even numbers = %d\n", even);
+    printf("Odd numbers = %d\n", odd);
+
+    return 0;
+}
