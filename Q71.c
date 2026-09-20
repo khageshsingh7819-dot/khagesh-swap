42790eb4a8d68773683266191caa492514de6a7b
khageshsingh7819-dot <khageshsingh7819@gmail.com>
2026-09-21 00:59:07 +0530

    Add files via upload

---
+++ Q71.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], rows, cols, i, j;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter matrix elements:\n");
+
+    // Reading the matrix
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Printing the matrix
+    printf("Matrix is:\n");
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
